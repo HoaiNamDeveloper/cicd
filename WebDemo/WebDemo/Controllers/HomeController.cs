2a0a03db582bca19bccc9e77035b5afba023d96d
HoaiNamDeveloper <phamhoainamcntt@gmail.com>
2024-04-15 10:54:02 +0700

    init

---
+++ WebDemo/WebDemo/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace WebDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            try
+            {
+                ViewBag.Message = "Your application description page.sql";
+
+            }
+            catch (SqlException e)
+            {
+                ViewBag.Message = e.Message.ToString();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = e.Message.ToString() + "|" + e.StackTrace?.Substring(0, 300);
+            }
+
+            return View();
+        }
+      public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
